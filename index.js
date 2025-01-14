f099ad51971dc9e4cf35d6e82ff1039118df051b
tykkit <125900587+tykkit@users.noreply.github.com>
2025-01-14 16:40:35 +0200

    added missing middleware (#4)

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('content', function getContent (request) {
     if (request.method === 'POST') {
